9c6c57e3eb256cffe7180ceef96897a070f74e28
donovanherion <donovanherion@gmail.com>
2020-10-11 14:45:56 +0200

    test share btn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
   navigator.share({
-    title: "Je suis un peu perdu laaaaa..",
-    url: "https://www.youtube.com/watch?v=wDjeBNv6ip0&ab_channel=LP",
+    title: document.title,
+    text: "Hello World",
+    url: "https://developer.mozilla.org",
   });
 });
