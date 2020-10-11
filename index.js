b6c836160e7b314fc7f13393a825185144aa7af9
donovanherion <donovanherion@gmail.com>
2020-10-11 14:50:36 +0200

    test share btn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
   navigator.share({
     title: "Je suis un peu perdu la",
+    text: "Je suis un peu perdu la",
     url: "https://www.youtube.com/watch?v=wDjeBNv6ip0&ab_channel=LP",
   });
 });
