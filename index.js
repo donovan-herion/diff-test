0620fee5a271d07c5e67c33df31cbdaa5dbacf8e
donovanherion <donovanherion@gmail.com>
2020-10-11 15:27:24 +0200

    test share btn audio

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-  navigator.canShare({
-    files: "./audio.mp3",
-  });
+  if (navigator.share) {
+    navigator
+      .share({
+        files: filesArray,
+        title: "Vacation Pictures",
+        text: "Photos from September 27 to October 14.",
+      })
+      .then(() => console.log("Share was successful."))
+      .catch((error) => console.log("Sharing failed", error));
+  } else {
+    console.log(`Your system doesn't support sharing files.`);
+  }
 });
